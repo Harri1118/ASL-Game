ec2f894217f69100b171406b695e83e07fe15c3e
Harri1118 <52708099+Harri1118@users.noreply.github.com>
2024-03-23 13:10:04 -0400

    More changes

--- makePrediction.py
+++ makePrediction.py
@@ -17,6 +17,8 @@ import keras
 
 base_model = tf.keras.applications.xception.Xception(weights= 'imagenet' ,include_top = False , input_shape = (150,150,3) , pooling = 'max' )
 
+alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', 'de', 'nothing', 'space']
+
 model = Sequential([
    base_model, 
            BatchNormalization(),
@@ -28,24 +30,22 @@ model = Sequential([
 #print(tf.version.VERSION)
 model.load_weights('Model/ASLModel.h5')
 
-# Load and preprocess the image
-img_path = 'Images/A_test.jpg'
-img = image.load_img(img_path, target_size=(150, 150))
-img_array = image.img_to_array(img)
-img_array = np.expand_dims(img_array, axis=0)
-img_array = preprocess_input(img_array)
+def predictImage():
+    # Load and preprocess the image
+    img_path = 'Images/capture.jpg'
+    img = image.load_img(img_path, target_size=(150, 150))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = preprocess_input(img_array)
+    # Make prediction
+    prediction = model.predict(img_array)
+    # Find the index of the class with the highest probability
+    predicted_class_index = np.argmax(prediction)
+    # Get the corresponding probability
+    highest_probability = prediction[0, predicted_class_index]
+    # Print the index and probability
+    print("Predicted class index:", alphabet[predicted_class_index])
+    print("Highest probability:", highest_probability)
+    return alphabet[predicted_class_index]
+    
 
-alphabets = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', 'de', 'nothing', 'space']
-
-# Make prediction
-prediction = model.predict(img_array)
-
-# Find the index of the class with the highest probability
-predicted_class_index = np.argmax(prediction)
-
-# Get the corresponding probability
-highest_probability = prediction[0, predicted_class_index]
-
-# Print the index and probability
-print("Predicted class index:", alphabets[predicted_class_index])
-print("Highest probability:", highest_probability)
